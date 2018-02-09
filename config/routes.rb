589ba707b8658a46bdb7437ad2799247c0ffd3a9
cwy007 <chanweiyan007@gmail.com>
2018-02-09 22:27:42 +0800

    add user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  get 'login',  to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [:create] 
+  resources :sessions, only: [:create]
   root to: 'welcome#index'
 end
